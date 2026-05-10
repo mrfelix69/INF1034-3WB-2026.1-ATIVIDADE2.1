e21216eb536aa759fedaefe48e0a38f508b59600
Bernardo Felix <berzk9oficial@gmail.com>
2026-05-10 18:40:55 -0300

    forca_atualizada

--- atividade7/forcapygame.py
+++ atividade7/forcapygame.py
@@ -1,46 +1,185 @@
-import pygame
+from pygame import *
 import random
 
-pygame.init()
+init()
 
-tela = pygame.display.set_mode((600, 400))
-font = pygame.font.SysFont(None, 40)
+window = display.set_mode((1200, 700))
+display.set_caption("Jogo da Forca")
 
-palavras = ['hamburguer','sushi','pizza','feijoada','arroz','carne','lasanha']
-palavra = random.choice(palavras)
+fonte = font.SysFont(None, 40)
+fonte_pequena = font.SysFont(None, 30)
+fonte_grande = font.SysFont(None, 60)
 
-letras = []
-vidas = 6
-texto = ''
+def desenhar_forca(vidas):
 
-def desenhar():
-    tela.fill((0,0,0))
+    draw.line(window, (0,0,0), (800,500), (950,500), 5)
+    draw.line(window, (0,0,0), (875,500), (875,120), 5)
+    draw.line(window, (0,0,0), (875,120), (950,120), 5)
+    draw.line(window, (0,0,0), (950,120), (950,150), 5)
 
-    exibicao = ''
-    for l in palavra:
-        exibicao += l + ' ' if l in letras else '_ '
+    if vidas <= 5:
+        draw.circle(window, (0,0,0), (950,180), 30, 3)
 
-    txt = font.render(exibicao, True, (255,255,255))
-    tela.blit(txt, (50, 200))
+    if vidas <= 4:
+        draw.line(window, (0,0,0), (950,210), (950,320), 3)
 
-    vidas_txt = font.render(f'Vidas: {vidas}', True, (255,0,0))
-    tela.blit(vidas_txt, (50,50))
+    if vidas <= 3:
+        draw.line(window, (0,0,0), (950,240), (900,280), 3)
 
-    pygame.display.update()
+    if vidas <= 2:
+        draw.line(window, (0,0,0), (950,240), (1000,280), 3)
 
-rodando = True
-while rodando:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            rodando = False
+    if vidas <= 1:
+        draw.line(window, (0,0,0), (950,320), (900,380), 3)
 
-        if event.type == pygame.KEYDOWN:
-            letra = event.unicode.lower()
-            if letra.isalpha():
-                if letra not in palavra:
-                    vidas -= 1
-                letras.append(letra)
+    if vidas <= 0:
+        draw.line(window, (0,0,0), (950,320), (1000,380), 3)
 
-    desenhar()
+def jogo_forca():
 
-pygame.quit()
+    palavras = [
+        'hamburguer',
+        'sushi',
+        'pizza',
+        'feijoada',
+        'arroz',
+        'carne',
+        'lasanha'
+    ]
+
+    while True:
+
+        palavra = random.choice(palavras)
+
+        letras = []
+
+        vidas = 6
+
+        venceu = False
+        perdeu = False
+
+        jogando = True
+
+        while jogando:
+
+            window.fill((180,220,255))
+
+            exibicao = ''
+
+            for l in palavra:
+
+                if l in letras:
+                    exibicao += l + ' '
+                else:
+                    exibicao += '_ '
+
+            txt = fonte_grande.render(exibicao, True, (0,0,0))
+
+            vidas_txt = fonte.render(
+                f'Vidas restantes: {vidas}',
+                True,
+                (255,0,0)
+            )
+
+            letras_txt = fonte.render(
+                f'Letras usadas: {" ".join(letras)}',
+                True,
+                (0,0,0)
+            )
+
+            info = fonte_pequena.render(
+                "Pressione ESC para voltar ao menu",
+                True,
+                (0,0,0)
+            )
+
+            window.blit(txt, (120,300))
+            window.blit(vidas_txt, (120,100))
+            window.blit(letras_txt, (120,160))
+            window.blit(info, (20,20))
+
+            desenhar_forca(vidas)
+
+            if '_' not in exibicao:
+                venceu = True
+                jogando = False
+
+            if vidas <= 0:
+                perdeu = True
+                jogando = False
+
+            display.update()
+
+            for ev in event.get():
+
+                if ev.type == QUIT:
+                    quit()
+
+                if ev.type == KEYDOWN:
+
+                    if ev.key == K_ESCAPE:
+                        return
+
+                    letra = ev.unicode.lower()
+
+                    if letra.isalpha():
+
+                        if letra not in letras:
+
+                            letras.append(letra)
+
+                            if letra not in palavra:
+                                vidas -= 1
+
+        # TELA FINAL
+        esperando = True
+
+        while esperando:
+
+            window.fill((20,20,20))
+
+            if venceu:
+                texto = fonte_grande.render(
+                    "VOCE VENCEU!",
+                    True,
+                    (0,255,0)
+                )
+            else:
+                texto = fonte_grande.render(
+                    f'VOCE PERDEU! Palavra: {palavra}',
+                    True,
+                    (255,0,0)
+                )
+
+            info1 = fonte.render(
+                "ENTER para jogar novamente",
+                True,
+                (255,255,255)
+            )
+
+            info2 = fonte.render(
+                "ESC para sair",
+                True,
+                (255,255,255)
+            )
+
+            window.blit(texto, (220,250))
+            window.blit(info1, (320,400))
+            window.blit(info2, (320,470))
+
+            display.update()
+
+            for ev in event.get():
+
+                if ev.type == QUIT:
+                    quit()
+
+                if ev.type == KEYDOWN:
+
+                    if ev.key == K_RETURN:
+                        esperando = False
+
+                    if ev.key == K_ESCAPE:
+                        quit()
+
+jogo_forca()
